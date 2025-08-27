f872ffb908290b1faaa7b2b848f175a47e727caa
HuntOreo <hloreair@outlook.com>
2025-08-27 00:18:38 -0400

    File hierarchy & Build book constructor, function to instantiate and store book

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('Hi world!')
+/*
+
+  Build Book constructure
+    The Book objects should have a unique id
+      generate id using crypto.randomUUID()
+
+  Declare a book function that takes some args
+    use provided args to instantiate Book objects, then store inside an array
+
+*/
+
+let bookShelf = [];
+
+function Book(name, author, publishDate) {
+  this.name = name;
+  this.author = author;
+  this.publishDate = publishDate;
+  this.id = crypto.randomUUID();
+}
+
+function storeBook(name, author, publishDate) {
+  const book = new Book(name, author, publishDate);
+  
+  bookShelf.push(book);
+}
+
+storeBook('Dracula', 'Bram Stoker', 1897);
+
+console.log(bookShelf);

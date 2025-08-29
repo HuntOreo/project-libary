ee6960271942e985ccf5dab034572afe7014f53d
HuntOreo <hloreair@outlook.com>
2025-08-28 20:19:44 -0400

    Add book covers and some light styling

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Library - Odin Project
 
 By Hunter Oreair, 2025
+
+## Credits
+Book covers from [librarything](https://www.librarything.com)
--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 let bookShelf = [];
 
-function Book(title, author, publishDate) {
+function Book(title, author, publishDate, cover) {
   this.title = title;
   this.author = author;
   this.publishDate = publishDate;
+  this.cover = cover;
   this.id = crypto.randomUUID();
 }
 
-function storeBook(title, author, publishDate) {
-  const book = new Book(title, author, publishDate);
-  
+function storeBook(title, author, publishDate, cover) {
+  const book = new Book(title, author, publishDate, cover);
+
   bookShelf.push(book);
 }
 
+// Render books onto page
 function displayBooks() {
   const body = document.querySelector('body');
   const gridContainerElm = document.createElement('div');
@@ -25,30 +27,60 @@ function displayBooks() {
     const cardElm = document.createElement('div');
     const cardContainerElm = document.createElement('div');
     const titleElm = document.createElement('h2');
+    const coverElm = document.createElement('img');
     const authorElm = document.createElement('p');
     const publishDateElm = document.createElement('p');
 
     cardElm.classList.toggle('card');
     cardContainerElm.classList.toggle('container');
     titleElm.classList.toggle('title');
+    coverElm.classList.toggle('cover');
 
     titleElm.innerText = book.title;
     authorElm.innerText = book.author;
     publishDateElm.innerText = book.publishDate;
+    console.log(book.cover);
+    coverElm.src = book.cover;
 
     gridContainerElm.appendChild(cardElm);
     cardElm.appendChild(cardContainerElm);
     cardContainerElm.appendChild(titleElm);
+    cardContainerElm.appendChild(coverElm);
     cardContainerElm.appendChild(authorElm);
     cardContainerElm.appendChild(publishDateElm);
   }
 }
 
-storeBook('Dracula', 'Bram Stoker', 1897);
-storeBook('The Hunger Games', 'Suzanne Collins', 2008);
-storeBook('The Catcher in the Rye', 'J. D. Salinger', 1951);
-storeBook('Lolita', 'Vladimir Nabokov', 1955);
-storeBook('The Lightning Thief', 'Rick Riordan', 2005);
+storeBook(
+  'Dracula',
+  'Bram Stoker',
+  1897,
+  'https://images-na.ssl-images-amazon.com/images/P/014143984X.01._SX450_SY635_SCLZZZZZZZ_.jpg'
+);
+storeBook(
+  'The Hunger Games',
+  'Suzanne Collins',
+  2008,
+  'https://images-na.ssl-images-amazon.com/images/P/0439023521.01._SX450_SY635_SCLZZZZZZZ_.jpg'
+);
+storeBook(
+  'The Catcher in the Rye',
+  'J. D. Salinger',
+  1951,
+  'https://images-na.ssl-images-amazon.com/images/P/0316769177.01._SX450_SY635_SCLZZZZZZZ_.jpg'
+);
+storeBook(
+  'Lolita',
+  'Vladimir Nabokov',
+  1955,
+  'https://images-na.ssl-images-amazon.com/images/P/0679723161.01._SX450_SY635_SCLZZZZZZZ_.jpg'
+);
+storeBook(
+  'The Lightning Thief',
+  'Rick Riordan',
+  2005,
+  'https://images-na.ssl-images-amazon.com/images/P/0786838655.01._SX450_SY635_SCLZZZZZZZ_.jpg'
+);
 
 
 displayBooks();
